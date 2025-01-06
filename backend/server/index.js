2a92a95f1154813802cf6bcc6da8b6e3f4f6e1a5
Anne <anne12080901@gmail.com>
2025-01-05 20:37:59 -0800

    cors

--- backend/server/index.js
+++ backend/server/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     next()
 });
 
-app.use(cors())
+app.use(cors({ origin: 'https://wastely.vercel.app' }))
 
 app.get('/all', async (req, res) => {
     const queryParams = new URLSearchParams({
